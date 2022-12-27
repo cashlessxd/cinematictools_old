24542c9ac2d264f0d91607c7a205f36bf2df1364
cashlessxd <mnoel8393@gmail.com>
2022-12-27 01:08:45 +0100

    fixed default values in Main.java

--- src/starter/Main.java
+++ src/starter/Main.java
@@ -10,14 +10,14 @@ public class Main {
 
         //keep in mind the default stick deadzone is between 39-61
         int leftStickX = 50;
-        int leftStickY = 39;
+        int leftStickY = 50;
         int rightStickX = 50;
-        int rightStickY = 39;
+        int rightStickY = 50;
 
         char focusKey = 'X';
 
         int coolDownDuration = 2000; //in milliseconds;
-        int movementLength = 1000; //in milliseconds;
+        int movementLength = 100; //in milliseconds;
         int framesPerSecond = 25;
         int videoLength = 1; //in seconds
 
